15dc85a59f6627a6bc644fb5b3a1b1a207edd18d
liamondrop <liamondrop@gmail.com>
2013-05-18 19:19:02 -0400

    add scripts field to package.json

--- example/app.js
+++ example/app.js
@@ -24,5 +24,5 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('> listening on http://127.0.0.1:' + app.get('port'));
 });
